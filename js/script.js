f2c63e8433d5a4036e72c6e7fb3930b0cd7c1e02
yusuf0786 <yusssu0786@gmail.com>
2021-02-27 12:37:34 +0530

    index.html

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+    $("header").hover(function(){
+      $("header .buttons a, header h2, header nav ul li a").css("color", "black");
+    },
+    function(){
+        $("header .buttons a, header h2, header nav ul li a").css("color","white");
+        }
+    );
+
+    $("header").hover(function(){
+
+        $("header h2").animate({
+            fontSize:"38px"
+        });
+    },
+    function(){
+        $("header h2").animate({
+            fontSize:"32px"
+        });
+    }
+    );
+
+
+    $("#usd-btn").click(function(){
+
+        $(".display-none").toggle();
+    });
+
+    // nav toggle
+    $(".nav-open").click(function(){
+
+        $("header nav").fadeIn(0500);
+        $(".nav-open").css('transform','rotate(90deg)');
+        $(".nav-open").animate({
+            opacity:'0'
+        },-1000);
+    });
+    $(".nav-close").click(function(){
+
+        $("header nav").hide();
+        $(".nav-open").css('transform','rotate(-1deg)');
+        $(".nav-open").animate({
+            opacity:'1'
+        },-1000);
+    });
